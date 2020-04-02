e4ce28bbbd83a80c0e0c2de50d2596abdafee190
dnlml <danielemeli86@gmail.com>
2020-04-02 18:33:41 +0200

    Adding script to backup las 10 days of db

--- backup.sh
+++ backup.sh
@@ -1,8 +1,12 @@
 export $(grep -v '^#' .env | xargs -d '\n')
 
-backup_name=$(date "+%d_%m_%Y_%H_%M_%S")
+BACKUP_FOLDER='./db_backup/'
+BACKUP_NAME=$(date "+%d_%m_%Y_%H_%M_%S")
+
+find "$BACKUP_FOLDER" -mtime +10 -type f -delete
 
 docker exec ${PMA_HOST} /usr/bin/mysqldump \
   -u ${MYSQL_USER} --password=${MYSQL_PASSWORD} \
-  ${MYSQL_DATABASE} > "$MYSQL_DATABASE"_"$backup_name".sql
+  ${MYSQL_DATABASE} | gzip > "$BACKUP_FOLDER""$MYSQL_DATABASE"_"$BACKUP_NAME".sql.gz
+
 unset $(grep -v '^#' .env | sed -E 's/(.*)=.*/\1/' | xargs -d '\n')
