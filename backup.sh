220ed6601b3999b25999c82c8c6ce9e2df1460ed
dnlml <danielemeli86@gmail.com>
2020-04-14 11:55:22 +0200

    Fixing delete older backup

--- backup.sh
+++ backup.sh
@@ -6,10 +6,10 @@ cd $SITE_PATH
 
 export $(grep -v '^#' .env | xargs -d '\n')
 
-find "$BACKUP_FOLDER" -mtime +10 -type f -delete
+find "$SITE_PATH""$BACKUP_FOLDER" -mtime +9 -type f -delete
 
 docker exec ${PMA_HOST} /usr/bin/mysqldump \
   -u ${MYSQL_USER} --password=${MYSQL_PASSWORD} \
-  ${MYSQL_DATABASE} | gzip > "$BACKUP_FOLDER""$MYSQL_DATABASE"_"$BACKUP_NAME".sql.gz
+  ${MYSQL_DATABASE} | gzip > ."$BACKUP_FOLDER""$MYSQL_DATABASE"_"$BACKUP_NAME".sql.gz
 
 unset $(grep -v '^#' .env | sed -E 's/(.*)=.*/\1/' | xargs -d '\n')
